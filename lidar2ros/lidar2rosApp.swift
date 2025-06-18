8e2abc38ab7b2f76af24edee3f61a924eaca9746
belle hsieh <hsiehannabelle2024@gmail.com>
2025-06-17 17:54:45 -0700

    Initial Commit

---
+++ lidar2ros/lidar2rosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidar2rosApp.swift
+//  lidar2ros
+//
+//  Created by belle hsieh on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct lidar2rosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
